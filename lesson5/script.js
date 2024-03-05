095b1ca5fb69fa9ad60de530f436a302d23924b4
Your Name <you@example.com>
2024-03-05 14:25:01 +0200

    added

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,3 +1,17 @@
 function welcome() {
     alert("ברוכים הבאים");
 }
+
+function welcome2() {
+    console.log("שלום לכולם");
+}
+
+function welcomeMessage() {
+    var name = document.getElementById("userName").value;
+
+    alert("אהלן " + name + ". נחמד!");
+}
+
+function multi50() {
+    
+}
